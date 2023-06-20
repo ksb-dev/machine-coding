45d786e5e3d0b8336732e9ccf1b569af1fcb2a71
Kedar Babaleshwar <babaleshwarkedar@gmail.com>
2023-06-20 12:03:38 +0530

    Initial Commit

--- 3-infinite-scroll/script.js
+++ 3-infinite-scroll/script.js
@@ -1,95 +1,68 @@
-;(function () {
-  const quotesEl = document.querySelector('.quotes')
-  const loaderEl = document.querySelector('.loader')
+let page = 1
+let totalPages = 0
+let limit = 10
 
-  // get the quotes from API
-  const getQuotes = async (page, limit) => {
-    const API_URL = `https://api.javascripttutorial.net/v1/quotes/?page=${page}&limit=${limit}`
-    const response = await fetch(API_URL)
-    // handle 404
-    if (!response.ok) {
-      throw new Error(`An error occurred: ${response.status}`)
-    }
-    return await response.json()
+const quotesEl = document.querySelector('.quotes')
+const loaderEl = document.querySelector('.loader')
+
+const showLoader = () => {
+  loaderEl.classList.remove('hide')
+}
+const hideLoader = () => {
+  loaderEl.classList.add('hide')
+}
+
+const getQuotes = async (page = 1, limit = 10) => {
+  const API_URL = `https://api.javascripttutorial.net/v1/quotes/?page=${page}&limit=${limit}`
+  const response = await fetch(API_URL)
+
+  if (!response.ok) {
+    throw new Error(`An error occurred: ${response.status}`)
   }
+  return await response.json()
+}
 
-  // show the quotes
-  const showQuotes = quotes => {
-    quotes.forEach(quote => {
-      const quoteEl = document.createElement('div')
-      quoteEl.classList.add('quote')
+const showQuotes = quotes => {
+  quotes.forEach(quote => {
+    const quoteEl = document.createElement('div')
+    quoteEl.classList.add('quote')
 
-      quoteEl.innerHTML = `
+    quoteEl.innerHTML = `
             <span>${quote.id})</span>
             ${quote.quote}
             <footer>-${quote.author}</footer>
         `
 
-      quotesEl.appendChild(quoteEl)
-    })
+    quotesEl.appendChild(quoteEl)
+  })
+}
+
+const loadQuotes = async () => {
+  showLoader()
+
+  var res
+
+  if (page === 1) {
+    res = await getQuotes()
+    totalPages = Math.ceil(res.total / limit)
+    showQuotes(res.data)
+  } else if (page > 1 && page <= totalPages) {
+    res = await getQuotes(page, limit)
+    showQuotes(res.data)
   }
 
-  const hideLoader = () => {
-    loaderEl.classList.add('hide')
+  hideLoader()
+
+  page++
+}
+
+window.addEventListener('scroll', () => {
+  if (
+    Math.ceil(window.scrollY + window.innerHeight) >=
+    document.documentElement.scrollHeight
+  ) {
+    loadQuotes()
   }
+})
 
-  const showLoader = () => {
-    loaderEl.classList.remove('hide')
-  }
-
-  const hasMoreQuotes = (page, limit, total) => {
-    const startIndex = (page - 1) * limit + 1
-    return total === 0 || startIndex < total
-  }
-
-  // load quotes
-  const loadQuotes = async (page, limit) => {
-    // show the loader
-    showLoader()
-
-    // 0.5 second later
-    setTimeout(async () => {
-      try {
-        // if having more quotes to fetch
-        if (hasMoreQuotes(page, limit, total)) {
-          // call the API to get quotes
-          const response = await getQuotes(page, limit)
-          // show quotes
-          showQuotes(response.data)
-          // update the total
-          total = response.total
-        }
-      } catch (error) {
-        console.log(error.message)
-      } finally {
-        hideLoader()
-      }
-    }, 500)
-  }
-
-  // control variables
-  let currentPage = 1
-  const limit = 10
-  let total = 0
-
-  window.addEventListener(
-    'scroll',
-    () => {
-      const { scrollTop, scrollHeight, clientHeight } = document.documentElement
-
-      if (
-        scrollTop + clientHeight >= scrollHeight - 5 &&
-        hasMoreQuotes(currentPage, limit, total)
-      ) {
-        currentPage++
-        loadQuotes(currentPage, limit)
-      }
-    },
-    {
-      passive: true
-    }
-  )
-
-  // initialize
-  loadQuotes(currentPage, limit)
-})()
+loadQuotes()
